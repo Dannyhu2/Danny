9696e7aa1d07236eba49c5d602dc9d09ce242d4b
Danny Hu <974517@learn.vsb.bc.ca>
2021-02-22 14:23:09 -0800

    game loop assignment added

--- Game loop assignment/src/Main.java
+++ Game loop assignment/src/Main.java
@@ -4,10 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int w;
+        int l;
+        w = 0;
+        l = 0;
         while (true) {
             //Starts scanner
             Scanner scanner = new Scanner(System.in);
             //Makes a message to tell the person to type rock paper or scissor
+            System.out.println("+==========================================+");
             System.out.println("Type in rock paper or scissor");
             //Makes a string called choice and choice will be what the user inputs into the scanner
             String choice = scanner.next();
@@ -26,29 +31,44 @@ public class Main {
                 if (random == 3) {
                     opponentMove = "scissor";
                 }
-                System.out.println("computer chose: " + opponentMove);
+
+                System.out.println("Computer chose: " + opponentMove + "      You chose: "+ choice);
                 //Checks if the outcome will be a tie.
                 if ((choice.equals(opponentMove))) {
                     System.out.println("It's a tie.");
+                    System.out.println("Wins: " + w + " Losses: " + l);
                 }
                 //Checks all possible outcomes and prints if it is a win or lose for the user playing.
                 if (choice.equals("rock") && opponentMove.equals("paper")) {
                     System.out.println("You Lose");
+                    l = l + 1;
+                    System.out.println("Wins: " + w + " Losses: " + l);
                 }
                 if (choice.equals("rock") && opponentMove.equals("scissor")) {
                     System.out.println("You Win");
+                    w = w + 1;
+                    System.out.println("Wins: " + w + " Losses: " + l);
                 }
                 if (choice.equals("paper") && opponentMove.equals("rock")) {
                     System.out.println("You Win");
+                    w = w + 1;
+                    System.out.println("Wins: " + w + " Losses: " + l);
                 }
                 if (choice.equals("paper") && opponentMove.equals("scissor")) {
                     System.out.println("You Lose");
+                    l = l + 1;
+                    System.out.println("Wins: " + w + " Losses: " + l);
                 }
                 if (choice.equals("scissor") && opponentMove.equals("rock")) {
                     System.out.println("You Lose");
+                    l = l + 1;
+                    System.out.println("Wins: " + w + " Losses: " + l);
                 }
                 if (choice.equals("scissor") && opponentMove.equals("paper")) {
                     System.out.println("You Win");
+                    w = w + 1;
+                    System.out.println("Wins: " + w + " Losses: " + l);
+
                 }
                 //If the user input from line 14 is wrong it will print this line
             } else {
