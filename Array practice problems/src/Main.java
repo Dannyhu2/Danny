3a61c1e7dd1107a9cda4ceedcb44afdd386bb5f3
Danny.Hu <78882308+Dannyhu2@users.noreply.github.com>
2021-02-24 11:35:01 -0800

    .

--- Array practice problems/src/Main.java
+++ Array practice problems/src/Main.java
@@ -8,29 +8,40 @@ public class Main {
                 count = count + 1;
             }
 
+
             //i < array.length; i++ is a boolean
             // i++ takes the variable and adds it by 1
 
             System.out.println(count);
         }
+        System.out.println("there were " + count + " ones in this array");
+        System.out.println(array[2]);
         System.out.println("************");
 
 
         int[] array2 = new int[10];
-        for (int i = 0; i < array2.length; i++) {
-            array2[i] = (int) (Math.random() * 100 + 1);
+        {
             int average = 0;
             int total = 0;
-            for (int num : array2) {
-                total = total + num;
-            }
-            System.out.println(total);
-            
-        }
-        System.out.println("**************");
-    }
-}
+            for (int i = 0; i < array2.length; i++) {
+                array2[i] = (int) (Math.random() * 100 + 1);
+                total = total + array2[i];
+                System.out.println(total);
 
+            }
+
+            System.out.println("total is: " + total);
+            System.out.println("Average is " + (total / 10));
+            System.out.println("****************");
+            System.out.println(array2[3]);
+
+
+
+        }
+
+    }
+
+}
 
 
 
