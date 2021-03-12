94fe5e862efb852a10829f87033f837f57bbf5ec
Danny Hu <974517@learn.vsb.bc.ca>
2021-03-12 15:01:19 -0800

    .

--- Time Class/src/Main.java
+++ Time Class/src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[]args){
+        Time clock = new Time();
+
+        clock.setTime(2,59,59);
+        clock.nextSecond();
+        clock.previousSecond();
+        clock.nextSecond();
+        clock.nextSecond();
+        System.out.println(clock);
+
+    }
 }
--- Time Class/src/Time.java
+++ Time Class/src/Time.java
@@ -3,6 +3,8 @@ public class Time {
     private int minute = 0;
     private int second = 0;
 
+
+
     public int getHour() {
         return hour;
     }
@@ -15,23 +17,70 @@ public class Time {
         return second;
     }
     public void setHour(int newHour){
+        if (newHour>24){
+            newHour = 0;
+        }
         hour = newHour;
+
     }
     public void setMinute(int newMinute){
+        if (newMinute>59){
+            newMinute = 0;
+
+        }
         minute = newMinute;
     }
     public void setSecond(int newSecond){
+        if (newSecond>59){
+            newSecond = 0;
+
+        }
         second = newSecond;
     }
     public void setTime(int newH, int newM, int newS){
         hour = newH;
         minute = newM;
         second = newS;
+
     }
-    public String toString(){
-        if( second <10 && second >0){
-            second;
+    public int nextSecond(){
+        second = second+1;
+        if(second>59) {
+            second = 0;
+            minute = minute + 1;
         }
+        if (minute > 59) {
+            minute = 0;
+            hour = hour + 1;
+        }
+        if (hour > 24){
+            hour = 0;
+        }
+        if (hour < 10){
+            
+        }
+        return second;
+    }
+    public int previousSecond(){
+        second = second-1;
+        if(second < 0){
+            second = 59;
+            minute = minute -1;
+        }
+        if(minute < 0){
+            minute = 59;
+            hour = hour -1;
+        }
+        if(hour < 0){
+            hour = 24;
+            second = 59;
+
+        }
+        return second;
+    }
+
+
+    public String toString(){
         return "\n Time: "+hour + ":" + minute + ":"+ second;
     }
 }
